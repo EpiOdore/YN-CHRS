211133e7c6cb06f92a4a58238a038eb52fb45892
AlxCzl <alchazal76@gmail.com>
2021-04-05 19:04:43 +0200

    feat(pre_treatment): removed unused variable

--- src/pre_treatment.py
+++ src/pre_treatment.py
@@ -7,7 +7,7 @@ def aleaGauss(sigma):
     return sigma*math.sqrt(-2*math.log(U1))*math.cos(2*math.pi*U2)
 
 def addBruitGaussien(dictionnary):
-    for key, value in dictionnary.items():
+    for _, value in dictionnary.items():
         for i in range(len(value[0])):
             for j in range(len(value[0][i])):
                 value[0][i][j] = value[0][i][j] + aleaGauss(0.025)
