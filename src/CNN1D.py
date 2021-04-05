e99f3b06f211ac521cac2ab98034ee2e36c3899f
AlxCzl <alchazal76@gmail.com>
2021-04-05 19:03:15 +0200

    feat(CNN1D): removed deadcode

--- src/CNN1D.py
+++ src/CNN1D.py
@@ -12,13 +12,9 @@ def pack_trames(trames, nb_pack):
         if i == 0:
             continue
         pack = trames[start:i]
-        # for trame in pack:
-        #     for j in range(len(trame)):
-        #         trame[j] = np.array(trame[j])
         packs.append(np.array(pack))
         start = i
-    # packs = np.array(packs)
-    # print("test")
+
     return packs
 
 
@@ -38,8 +34,10 @@ def trunc_dataset_1D(dico_trames, percent, nb_pack):
     cluster = float(0)
     for key, values in dico_trames.items():
         trames = values[0]
-        train_pack = pack_trames(trames[0: int(len(trames) * percent)], nb_pack)
-        test_pack = pack_trames(trames[int(len(trames) * (1 - percent)):], nb_pack)
+        train_pack = pack_trames(
+            trames[0: int(len(trames) * percent)], nb_pack)
+        test_pack = pack_trames(
+            trames[int(len(trames) * (1 - percent)):], nb_pack)
         train_trames += train_pack
         test_trames += test_pack
 
@@ -54,27 +52,16 @@ def trunc_dataset_1D(dico_trames, percent, nb_pack):
 
 def create_model(nb_pack):
     model = tf.keras.Sequential()
-    # model_m.add(Reshape((TIME_PERIODS, num_sensors), input_shape=(input_shape,)))
-    model.add(tf.keras.layers.Conv1D(17 * nb_pack, 2, activation='relu', input_shape=(nb_pack, 17)))
+    model.add(tf.keras.layers.Conv1D(17 * nb_pack, 2,
+              activation='relu', input_shape=(nb_pack, 17)))
     model.add(tf.keras.layers.Conv1D(17 * nb_pack, 2, activation='relu'))
     model.add(tf.keras.layers.MaxPooling1D(2))
     model.add(tf.keras.layers.Conv1D(80, 1, activation='relu'))
     model.add(tf.keras.layers.Conv1D(80, 1, activation='relu'))
     model.add(tf.keras.layers.GlobalAveragePooling1D())
     model.add(tf.keras.layers.Dropout(0.5))
-    # model.add(tf.keras.layers.Dense(17 * nb_pack, input_dim=17, activation='relu'))
-    # model.add(tf.keras.layers.Dense(17 * 2 * nb_pack, activation='relu'))
-    # model.add(tf.keras.layers.Dense(int(17 * nb_pack * 0.6 + 42), activation='relu'))
-    # model.add(tf.keras.layers.Dense(42, activation='relu'))
     model.add(tf.keras.layers.Dense(42, activation='sigmoid'))
-    # model.add(tf.keras.layers.Dense(42, activation='relu')) #-> Peut etre superflu
     model.add(tf.keras.layers.Dense(42, activation='softmax'))
-    # print(model.summary())
-
-    # model_callback = tf.keras.callbacks.ModelCheckpoint(filepath="./model_weight-" + str(nb_model),
-    #                                              save_weights_only=True,
-    #                                              verbose=1)
-    # print(truncated_dataset[0][0])
 
     model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), optimizer='adam',
                   metrics=['accuracy'])
@@ -87,13 +74,16 @@ def neural_network_1D(dico_trames, percent, nb_model, nb_pack, save=False):
 
     model = create_model(nb_pack)
 
-    model.fit(truncated_dataset[0], truncated_dataset[1], epochs=100, validation_data=(truncated_dataset[2], truncated_dataset[3]))
+    model.fit(truncated_dataset[0], truncated_dataset[1], epochs=100, validation_data=(
+        truncated_dataset[2], truncated_dataset[3]))
 
     if save:
         model.save("./model_weight-" + str(nb_model))
 
-    _, train_accuracy = model.evaluate(truncated_dataset[0], truncated_dataset[1])
-    _, test_accuracy = model.evaluate(truncated_dataset[2], truncated_dataset[3])
+    _, train_accuracy = model.evaluate(
+        truncated_dataset[0], truncated_dataset[1])
+    _, test_accuracy = model.evaluate(
+        truncated_dataset[2], truncated_dataset[3])
 
     print("Accuracy on train trames: ", train_accuracy)
     print("Accuracy on test trames: ", test_accuracy)
